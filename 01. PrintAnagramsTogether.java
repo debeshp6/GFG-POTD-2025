e48ac8a17a5be1be80c83247b152381c67e53f27
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-01-06 08:55:26 +0530

    Create 01. PrintAnagramsTogether.java

---
+++ 01. PrintAnagramsTogether.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ArrayList<ArrayList<String>> anagrams(String[] arr) {
+        
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        for(String str : arr) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+            
+            map.putIfAbsent(sortedStr, new ArrayList<>());
+            map.get(sortedStr).add(str);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
