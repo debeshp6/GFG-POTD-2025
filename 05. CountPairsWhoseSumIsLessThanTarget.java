d6541a21d5776cc51ee6d293dcd294f42abf6ef1
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-01-06 09:00:57 +0530

    Create 05. CountPairsWhoseSumIsLessThanTarget.java

---
+++ 05. CountPairsWhoseSumIsLessThanTarget.java
@@ -0,0 +1,20 @@
+// User function Template for Java
+class Solution {
+    int countPairs(int arr[], int target) {
+        
+        Arrays.sort(arr);
+        int n = arr.length;
+        int i = 0, j = n-1;
+        int cnt = 0;
+        
+        while(i < j) {
+            if(arr[i] + arr[j] < target) {
+                cnt += (j-i);
+                i++;
+            }
+            else j--;
+        }
+        
+        return cnt;
+    }
+}
