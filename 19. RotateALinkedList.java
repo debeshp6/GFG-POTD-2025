4cacc303d7d6caeeae3c1e686656c082729f0a6a
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-01-19 01:56:51 +0530

    19 January

--- 19. RotateALinkedList.java
+++ 19. RotateALinkedList.java
@@ -1,28 +1,41 @@
+/* node of linked list:
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+
+*/
+
 class Solution {
     public Node rotate(Node head, int k) {
-        
+        // add code here
         int lSize = size(head);
         k = k % lSize;
         Node reverseHead = reverseNode(head);
+        // reverse first lSize-k node 
         Node nextNode = null;
         Node curr = reverseHead;
         for (int i = 0; i < lSize-k-1; i++) {
             curr = curr.next;
         }
-
         nextNode = curr.next;
-        curr.next = null; 
-        Node reverseStartingNode = reverseNode(reverseHead);
-        Node reverseNextNode = reverseNode(nextNode);
-        Node temp = reverseStartingNode;
-        while (temp.next != null) {
+    curr.next = null; // disconnect the first part
+    Node reverseStartingNode = reverseNode(reverseHead);
+    Node reverseNextNode = reverseNode(nextNode);
+    // Find the last node of the first reversed segment
+    Node temp = reverseStartingNode;
+    while (temp.next != null) {
         temp = temp.next;
-        }
-        temp.next = reverseNextNode; // connect the two parts
-        return reverseStartingNode;
+    }
+    temp.next = reverseNextNode; // connect the two parts
+    return reverseStartingNode;
         
-        }
-
+    }
     private static Node reverseNode(Node head) {
         Node curr = head;
         Node prev = null;
@@ -34,7 +47,6 @@ class Solution {
         }
         return prev;
     }
-
     private static int size(Node node) {
         int count = 0;
         Node curr = node;
