f85ff25eba7b7e5bf9e68e2c78d47071b0733a14
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-01-21 01:17:12 +0530

    21 January

---
+++ 21. LinkedListGroupReverse.java
@@ -0,0 +1,38 @@
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution {
+    public static Node reverseKGroup(Node head, int k) {
+        
+        if(head == null) return null;
+        
+        Node nextNode = null;
+        Node curr = head;
+        Node prev = null;
+        int cnt = 0;
+        
+        while (curr != null && cnt < k) {
+            nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+            cnt++;
+        }
+        
+        if(nextNode != null)
+            head.next = reverseKGroup(nextNode,k);
+            
+        return prev;
+    }
+}
