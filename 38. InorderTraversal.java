751fd36cb9c74572f62aed6c8bbb53d69d204914
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-02-07 10:26:44 +0530

    7 February

---
+++ 38. InorderTraversal.java
@@ -0,0 +1,30 @@
+/* A Binary Tree node
+
+class Node {
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    // Function to return a list containing the inorder traversal of the tree.
+    void helper(Node root, ArrayList<Integer> arr) {
+        if(root==null) {
+            return ;
+        }
+        
+        helper(root.left, arr);
+        arr.add(root.data);
+        helper(root.right, arr);
+    }
+    
+    ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        helper(root,arr);
+        return arr;
+    }
+}
