0867a1d0fd1f7a6f319632b9f8f1436320842186
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-02-21 06:55:25 +0530

    21 February

---
+++ 52. ParenthesisChecker.java
@@ -0,0 +1,23 @@
+
+
+
+class Solution {
+    static boolean isBalanced(String s) {
+        
+        Stack<Character> st = new Stack<>();
+        
+        for(int i = 0 ; i< s.length() ; i++){
+            if(st.isEmpty()) st.push(s.charAt(i));
+            else if(st.peek() == '{' && s.charAt(i) == '}') st.pop();
+            else if(st.peek() == '(' && s.charAt(i) == ')') st.pop();
+            else if(st.peek() == '[' && s.charAt(i) == ']') st.pop();
+            else{
+                st.push(s.charAt(i));
+            }
+        }
+        
+        if(st.isEmpty()) return true;
+        return false;
+        
+    }
+}
