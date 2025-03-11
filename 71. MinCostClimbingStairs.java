75edb460bf336d9552b3edd45730c8e5e725467f
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-03-12 01:34:39 +0530

    12 March

---
+++ 71. MinCostClimbingStairs.java
@@ -0,0 +1,17 @@
+//Back-end complete function Template for Java
+
+class Solution {
+    static int minCostClimbingStairs(int[] cost) {
+        
+        int n = cost.length;
+        int[] dp = new int[n+1];
+      
+        for(int i = n-1; i >= 0; i--) {
+            int onestep = cost[i] + dp[i + 1];
+            int twostep = Integer.MAX_VALUE;
+            if(i + 2 <= n) twostep = cost[i] + dp[i +2];
+            dp[i] = Math.min(onestep, twostep);
+        }
+        return Math.min(dp[0], dp[1]);
+    }
+};
