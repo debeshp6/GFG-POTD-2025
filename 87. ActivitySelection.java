fc85cc7c379ffa7d5deb3e2da7ae2e87f8df535d
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-03-28 02:25:26 +0530

    28 March

---
+++ 87. ActivitySelection.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int activitySelection(int[] start, int[] end) {
+       
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        
+        for (int i = 0; i < start.length; ++i)
+            pq.offer(new int[] {start[i], end[i]});
+            
+        int cnt = 1;
+        int ans = pq.peek()[1];
+        
+        while (!pq.isEmpty()) {
+             if (pq.peek()[0] > ans) {
+                cnt++;
+                ans = pq.peek()[1];
+            }
+            pq.poll();
+        }
+        return cnt;
+    }
+}
