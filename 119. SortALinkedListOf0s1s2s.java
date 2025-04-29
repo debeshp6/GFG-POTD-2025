979fed5e1dc4b397f7b10126199a651e562cf76a
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-04-29 21:23:08 +0530

    29 April

---
+++ 119. SortALinkedListOf0s1s2s.java
@@ -0,0 +1,33 @@
+
+/* class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}*/
+
+class Solution {
+    static Node segregate(Node head) {
+        // code here
+         int[] freq=new int[3];
+        Node temp=head;
+        while(temp!=null){
+            freq[temp.data]++;
+            temp=temp.next;
+        }
+        temp=head;
+        for(int i=0;i<3;i++){
+            int count=i;
+            while(freq[count]--!=0){
+                temp.data=i;
+                temp=temp.next;
+            }
+        }
+        
+        return head;
+    }
+}
