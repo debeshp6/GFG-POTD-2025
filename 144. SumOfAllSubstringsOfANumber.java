c340a78d5d96f79d801e9d0779ea49b070b4ab6b
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-05-24 00:45:38 +0530

    24 May

---
+++ 144. SumOfAllSubstringsOfANumber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int sumSubstrings(String s) {
+       
+        int n = s.length();
+        int sum = 0;
+        int prev = 0;
+        
+        for(int i=0 ; i<n ; i++){
+            int num = 0;
+            int x = s.charAt(i) - '0';
+            prev = (prev*10) + x*(i+1);
+            sum += prev;
+        }
+        
+        return sum;
+    }
+}
