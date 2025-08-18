4671d718b2e4c4108bdb2121892303d5bed7e5ec
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-08-18 22:47:10 +0530

    18 August

---
+++ 230. FindH-Index.java
@@ -0,0 +1,21 @@
+
+
+class Solution {
+    
+    public int hIndex(int[] citations) {
+        
+        Arrays.sort(citations);
+        int n = citations.length; 
+        int hIndex = 0; 
+        
+        for (int i = 0; i < n; i++) {
+            int papersWithAtLeastCitations = n - i; 
+            if (citations[i] >= papersWithAtLeastCitations) {
+                hIndex = papersWithAtLeastCitations; 
+                break; 
+            }
+        }
+        
+        return hIndex;
+    }
+}
