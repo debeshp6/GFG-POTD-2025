8552c8058d51913a8aac3e50043d7724bab61fc8
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-12-19 03:05:06 +0530

    19 December

---
+++ 353. BusConductor.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMoves(int[] chairs, int[] passengers) {
+        
+        Arrays.sort(chairs);
+        Arrays.sort(passengers);
+        int sum  = 0;
+        
+        for(int i=0; i<chairs.length; i++) {
+            sum += Math.abs(chairs[i] - passengers[i]);
+        }
+        
+        return sum;
+
+        
+    }
+}
